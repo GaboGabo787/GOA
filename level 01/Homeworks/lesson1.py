af6693b5fa0a1bf23ed449d8404a638ce5bac15b
GaboGabo787 <gabikalanddze@gmail.com>
2024-05-14 19:08:48 +0400

    commit

---
+++ level 01/Homeworks/lesson1.py
@@ -0,0 +1,7 @@
+name = "Gabriel"         #სახელი
+surname = "kalandadze"    #გვარი
+Age = "11"  #ასაკი
+Height ="143"
+wants_programing = "wants_programing" 
+is_dumb = "is_dumb"
+print(name + " " + surname + " " + str(Age) + " " "წლის " + " " + str(Height) + " " "სანტიმეტრი" + " " + wants_programing + " " + is_dumb )

b0904793d29938ba684c419d1331bbd37e305b07
GaboGabo787 <gabikalanddze@gmail.com>
2024-05-25 14:09:04 +0400

    commit

---
+++ level03/homework/homework2.py
@@ -0,0 +1,15 @@
+print(150 + 565)
+print(15970 + 2012)
+
+print(1694 - 155)
+print(441 - 43)
+
+
+print(56 * 64)
+print(15 * 2)
+
+print(90 // 2)
+print(149 // 4)
+
+print(60 ** 60)
+print(1000 ** 50)

c357e76793d5b975fd061047856ca6e7df8236a3
GaboGabo787 <gabikalanddze@gmail.com>
2024-06-01 13:38:50 +0400

    commit

---
+++ level 04/classwork/level 04.py
@@ -0,0 +1,27 @@
+# 1) ცვლადის დასახელება
+# 2) ტოლობის სიმბოლო
+# 3) ცვლადის მნიშვნელობა
+
+
+#(1), (2) (3)
+name = 'Gabi'
+last_name = 'Kalandadze'
+profession = '3D Modeling'
+
+print(name + " " + last_name + " " +  profession)
+
+#Updating variable value
+
+name = 'dato'
+
+name = 'nikolozi'
+
+name = 'grisha'
+name = 'Angela'
+name = 100
+print(name)
+
+
+name = 'dato'
+last_name = 'janezashvili'
+print(name)
---
+++ level 04/homework/level 04.py
@@ -0,0 +1,7 @@
+name = 'Gabriel'
+last_name = 'Kalandadze'
+age = 11 
+hobby = '3D Modeling'
+country = 'Georgia'
+
+print(name + ' '+ last_name + ' ' + str(age) + ' '+ hobby + ' ' + country)
